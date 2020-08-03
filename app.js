6a2a56ad9162d932e2f17e6f957ca239d94425ac
aaron.harpt <aaron.harpt@gmail.com>
2020-08-03 18:59:22 -0500

    Worked on bootstrap, jquery

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ var express = require("express");
 var path = require("path");
 var app = express();
 
+app.use("/css", express.static(path.join(__dirname, "/node_modules/bootstrap/dist/css")));
+app.use("/js", express.static(path.join(__dirname, "/node_modules/bootstrap/dist/js")));
+
 app.get("/", function(request, response) {
   response.sendFile(path.join(__dirname, "public/index.html"));
 });
